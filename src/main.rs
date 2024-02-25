5caf6d878df849ede22ac854c81c698ee15c92ba
xenya52 <108221381+xenya52@users.noreply.github.com>
2024-02-25 16:17:05 +0100

    Outsorced the init table function and the url string, create the drop table function to delete the whole todo table, create a enum for future plans

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,20 @@ use dotenv::dotenv;
 use std::env;
 use tokio_postgres::{NoTls, Error};
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    // Load environment variables from .env file
-    dotenv().ok();
+enum todo_table_action {
+    init,
+    drop,
+    insert,
+    update,
+    delete,
+    merge,
 
-    // Construct the database connection string
+
+}
+
+fn get_database_url() -> String {
+    // Load environment variables from .env file & construct the database connection string
+    dotenv().ok();
     let database_url = format!(
         "host={} user={} dbname={} password={}",
         env::var("DB_HOST").expect("DB_HOST must be set"),
@@ -15,7 +23,10 @@ async fn main() -> Result<(), Error> {
         env::var("DB_NAME").expect("DB_NAME must be set"),
         env::var("DB_PASSWORD").expect("DB_PASSWORD must be set"),
     );
+    return database_url;
+}
 
+async fn init_table(database_url:String) -> Result<(), Error> {
     // Connect to the database
     let (client, connection) = tokio_postgres::connect(&database_url, NoTls).await?;
 
@@ -32,14 +43,47 @@ async fn main() -> Result<(), Error> {
         .execute(
             "CREATE TABLE IF NOT EXISTS todo (
                 id SERIAL PRIMARY KEY,
-                name VARCHAR NOT NULL,
-                description JSONB
+                task_name VARCHAR NOT NULL,
+                task_description TEXT
             )",
             &[],
         )
         .await?;
-
+    
     println!("Table created successfully");
+    Ok(())
+}
+
+async fn drop_table(database_url:String) -> Result<(), Error> {
+    // Connect to the database
+    let (client, connection) = tokio_postgres::connect(&database_url, NoTls).await?;
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    // Execute a statement to create a new table
+    client
+        .execute(
+            "DROP TABLE todo
+            ",
+            &[],
+        )
+        .await?;
+    
+    println!("Table dropped successfully");
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let url = get_database_url();
+
+    drop_table(url).await?;
 
     Ok(())
 }
