475ebca6619e1fa25c480e2cfeab10e4ef425e4c
xenya52 <108221381+xenya52@users.noreply.github.com>
2024-02-27 21:11:37 +0100

    make insert and show method

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use dotenv::dotenv;
-use std::env;
+use std::{env, future::Future};
 use tokio_postgres::{NoTls, Error};
+use std::fmt;
 
+struct Todo {
+    id: i32,
+    name: String,
+    description: String,
+}
+impl fmt::Display for Todo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "ID: {}, Name: {}, Description: {}", self.id, self.name, self.description)
+    }
+}
 enum todo_table_action {
+    show,
     init,
     drop,
     insert,
     update,
     delete,
     merge,
-
-
 }
 
 fn get_database_url() -> String {
@@ -78,12 +88,71 @@ async fn drop_table(database_url:String) -> Result<(), Error> {
     println!("Table dropped successfully");
     Ok(())
 }
+async fn insert_table(database_url:String) -> Result<(), Error> {
+    // Connect to the database
+    let (client, connection) = tokio_postgres::connect(&database_url, NoTls).await?;
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    // Execute a statement to create a new table
+    client
+        .execute(
+            "INSERT INTO public.todo
+            (id, task_name, task_description)
+            VALUES(nextval('todo_id_seq'::regclass), 'test_header', 'test_description');
+            ",
+            &[],
+        )
+        .await?;
+    
+    println!("Table dropped successfully");
+    Ok(())
+}
+async fn show_todo_table(database_url: String) -> Result<(), Error> {
+    // Connect to the database
+    let (client, connection) = tokio_postgres::connect(&database_url, NoTls).await?;
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    // Fetch and display the results
+    let rows = client
+        .query(
+            "SELECT * FROM todo",
+            &[],
+        )
+        .await?;
+
+    for row in rows {
+        let todo = Todo {
+            id: row.get(0),
+            name: row.get(1),
+            description: row.get(2),
+        };
+
+        // Print each Todo item
+        println!("{}", todo);
+    }
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let url = get_database_url();
 
-    drop_table(url).await?;
+    show_todo_table(url).await?;
 
     Ok(())
 }
