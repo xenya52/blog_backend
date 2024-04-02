a73f782ea75817dcde9ea9808e00683d2cf3e751
xenya52 <108221381+xenya52@users.noreply.github.com>
2024-04-02 19:08:47 +0200

    add exampleheader to my main

--- src/handlers.rs
+++ src/handlers.rs
@@ -5,7 +5,7 @@ use std::env;
 use crate::{Todo, executions, todo_table_actions};
 
 fn get_database_url() -> String {
-    // Load environment variables from .env file & construct the database connection string
+    
     dotenv().ok();
     let database_url = format!(
         "host={} user={} dbname={} password={}",
@@ -14,17 +14,19 @@ fn get_database_url() -> String {
         env::var("DB_NAME").expect("DB_NAME must be set"),
         env::var("DB_PASSWORD").expect("DB_PASSWORD must be set"),
     );
+
     return database_url;
 }
 
 pub async fn get_post(id: i32) -> Result<impl warp::Reply, warp::Rejection> {
     let url = get_database_url();
-    // Verwenden Sie .await hier, um auf das Ergebnis der executions Funktion zu warten
     let result = executions(url, todo_table_actions::Show).await;
     let mut database_content: Vec<Todo> = vec![];
+    
     match result {
         Ok(stuff) => database_content = stuff,
         Err(e) => panic!("{}", e),
     }
+
     Ok(warp::reply::json(&database_content))
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,25 @@ use tokio_postgres::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    // use insomnia !
     //todotable_to_jsonstring(url);
     // let _ = executions(url, todo_table_actions::Insert).await;
-    let routes = routes();
-
-    println!("Server started at http://localhost:8000");
-    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
-    Ok(())
+    // let routes = routes();
+    // println!("Server started at http://localhost:8000");
+    // warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+    // Ok(())
+    HttpServer::new(|| {
+        App::new()
+            .wrap(
+                Cors::default() // <-- Constructs the CORS middleware builder
+                .allowed_origin("http://example.com") // <-- Specify the allowed origin
+                .allowed_methods(vec!["GET", "POST"]) // <-- Specify the allowed methods
+                .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT]) // <-- Specify the allowed headers
+                .allowed_header(header::CONTENT_TYPE) // <-- You can specify individual headers
+                .max_age(3600), // <-- Optional: specify the max cache duration for the preflight request
+            )
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
