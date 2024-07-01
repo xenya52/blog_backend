7596e318bddea753b45dc72bbeba5156f3621bf8
xenya52 <666nikkineko@gmail.com>
2024-07-01 22:59:22 +0200

    backup commit add rocket, first that

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Todos
+ - First things first, follow the guide: <BS>https://dev.to/hackmamba/build-a-rest-api-with-rust-and-mongodb-rocket-version-ah5
  - Create db and collection
  - Connect to mongodb
  - Make a restful api
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,13 @@
-use mongodb::{Client, options::ClientOptions};
-use std::env;
-use std::error::Error;
-use dotenv::dotenv;
-use tokio;
+[macro_use]
+extern crate rocket;
+use rocket::{get, http::Status, serde::json::Json};
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-   dotenv().ok();
-   let client_options = ClientOptions::parse(env::var("MONGODB_URI").expect("You must set the MONGODB_URI env var!")).await.unwrap();
-   let client = Client::with_options(client_options).unwrap();
-   println!("{:?}", client);
-   // Load the MongoDB connection string from an environment variable:
-   // let client_uri =
-      // env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+#[get("/")]
+fn hello() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Hello from rust and mongoDB")))
+}
 
-   // A Client is needed to connect to MongoDB:
-   // An extra line of code to work around a DNS issue on Windows:
-   // let options =
-     // ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
-         // .await?;
-   // let client = Client::with_options(options)?;
-
-   // Print the databases in our MongoDB cluster:
-   // println!("Databases:");
-   // for name in client.list_database_names(None, None).await? {
-      // println!("- {}", name);
-   // }
-
-   Ok(())
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
 }
