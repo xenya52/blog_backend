ff22294dea28d2c23ecc334c86ba98efb1066603
xenya52 <666nikkineko@gmail.com>
2024-07-20 22:03:00 +0200

    git commit -m

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,27 @@ mod model;
 mod controller;
 
 use actix_web::web::Data;
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, Responder, get};
 use mongodb::Client;
 use dotenv::dotenv;
+
+#[actix_web::get("/hello")]
+async fn greet() -> impl Responder {
+    format!("Hello world")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    let port = 8080;
+    let host = "127.0.0.1";
     let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| "mongodb://localhost:27017".into());
 
     let client = Client::with_uri_str(uri).await.expect("failed to connect");
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(Data::new(client.clone()))
-    })
-    .bind(("127.0.0.1", 8080))?
+    HttpServer::new(move ||App::new())
+    .bind((host, port))?
+    .workers(2)
     .run()
     .await
 }
